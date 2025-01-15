19d73d4a29b09e417c6d3669fdbd67e73b3c8477
WorkWolf2 <81331630+WorkWolf2@users.noreply.github.com>
2025-01-16 00:59:09 +0100

    Removed token

--- src/main/java/com/minegolem/minebot/utils/Secret.java
+++ src/main/java/com/minegolem/minebot/utils/Secret.java
@@ -1,5 +1,5 @@
 package com.minegolem.minebot.utils;
 
 public class Secret {
-    public final static String TOKEN = "MTMyNzAyNjAyMjQ2MTIxMDY5NQ.GaDWDn.q9ncLYcADLAyWZY99Q7a1XWJEzIWuC6YBiOx1k";
+    public final static String TOKEN = "";
 }
